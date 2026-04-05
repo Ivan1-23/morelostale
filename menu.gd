0ad3b8756bb69345712595e55ac935c906440cf9
Ivan1-23 <iv4n.osorio@gmail.com>
2026-04-04 22:08:36 -0700

    más cambios en el menu

--- menu.gd
+++ menu.gd
@@ -7,12 +7,12 @@ extends CanvasLayer
 @onready var telefono = $Control/NinePatchRect/Telefono
 var select_x = 18
 var select_y = 28
-var px_d = 48
-enum ScreenLoaded {NOTHING, JUST_MENU, OBJECTO, ESTADISTICAS, TELEFONO}
+var px_d = 45
+enum ScreenLoaded {NOTHING, JUST_MENU, OBJECTO, ESTADISTICAS, TELEFONO,OBJETO2,INFO,CAJAa,CAJAb}
 var screen_loaded = ScreenLoaded.NOTHING
 var selected_option: int = 0
 var selected_option2: int = 0
-var inventario: Array[String] = []
+var selected_option3: int = 0
 
 func _ready():
 	menu.visible = false
@@ -61,7 +61,29 @@ func _unhandled_input(event):
 				else:
 					selected_option2 -= 1
 					select_arrow.position = Vector2(186,-96 + (selected_option2 % 8 * 36))
-				
+			elif event.is_action_pressed("acción"):
+				select_arrow.position = Vector2(188 + (selected_option3 % 3) * 100,195)
+				screen_loaded = ScreenLoaded.OBJETO2
+		ScreenLoaded.OBJETO2:
+			if event.is_action_pressed("acción") or event.is_action_pressed("correr") or event.is_action_pressed("menu"):
+				menu.visible = false
+				objetos.visible = false
+				select_arrow.position = Vector2(select_x, select_y + (selected_option % 3) * px_d)
+				screen_loaded = ScreenLoaded.NOTHING
+			elif event.is_action_pressed("acción") and selected_option3 == 2:
+				menu.visible = false
+				objetos.visible = false
+				select_arrow.position = Vector2(select_x, select_y + (selected_option % 3) * px_d)
+				screen_loaded = ScreenLoaded.INFO
+			elif event.is_action_pressed("ui_right"):
+				selected_option3 += 1
+				select_arrow.position = Vector2(188 + (selected_option3 % 3) * 100,195)
+			elif event.is_action_pressed("ui_left"):
+				if selected_option3 == 0:
+					select_arrow.position = Vector2(188 + (selected_option3 % 3) * 100,195)
+				else:
+					selected_option3 -=1
+					select_arrow.position = Vector2(188 + (selected_option3 % 3) * 100,195)
 		ScreenLoaded.ESTADISTICAS:
 			if event.is_action_pressed("menu") or event.is_action_pressed("correr"):
 				estadisticas.visible = false
