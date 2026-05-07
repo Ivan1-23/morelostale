6ced6a4cf544434edc5f190d6180164e74c3f28d
Ivan1-23 <160800794+Ivan1-23@users.noreply.github.com>
2026-05-06 20:05:52 -0700

    1.0.5.2

--- menu.gd
+++ menu.gd
@@ -6,7 +6,6 @@ extends CanvasLayer
 @onready var estadisticas = $Control/NinePatchRect/Estadisticas
 @onready var telefono = $Control/NinePatchRect/Telefono
 @onready var info = $Control2/info
-@onready var button1 = $Control/NinePatchRect/OBJT
 var select_x = 28
 var select_y = 35
 var px_d = 33
