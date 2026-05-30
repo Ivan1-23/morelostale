59eed611f2d01f8f452071cf7c432c21447ccd42
Ivan1-23 <iv4n.osorio@gmail.com>
2026-05-30 06:44:04 -0700

    Solucionado el aspecto de pantalla y controles en PC y celular

--- touch_button.gd
+++ touch_button.gd
@@ -1,7 +1,13 @@
 extends CanvasLayer
-
 @onready var controles = $Control
+var proxima_posicion_jugador: Vector2 = Vector2.ZERO
 
 func _ready():
-	if not DisplayServer.is_touchscreen_available():
-		controles.visible = false
+		# Detectamos si el jugador está en Android, iOS o Web Móvil
+			if OS.has_feature("mobile") or OS.has_feature("web_android") or OS.has_feature("web_ios"):
+				# Si es celular, forzamos el modo EXPAND para liberar los controles táctiles
+				get_tree().root.content_scale_aspect = Window.CONTENT_SCALE_ASPECT_EXPAND
+			else:
+			# Si es PC (Windows, Mac, Linux, Web de escritorio), forzamos KEEP para mantener el 4:3
+				get_tree().root.content_scale_aspect = Window.CONTENT_SCALE_ASPECT_KEEP
+				controles.visible = false
